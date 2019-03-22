ce622b22938d420b19befcf946113497a6002af8
Nathan Greene <nathangreene3@gmail.com>
2019-03-21 21:32:15 -0400

    Initial state

---
+++ game.go
@@ -0,0 +1,180 @@
+package main
+
+import "fmt"
+
+type action byte
+type mode byte
+type pawn byte
+type state byte
+type board [][]pawn
+
+type game struct {
+	b board
+	s state
+	m mode
+}
+
+const (
+	noPawn    = pawn(' ')
+	whitePawn = pawn('w')
+	blackPawn = pawn('b')
+
+	illegal state = iota
+	stalemate
+	whiteTurn
+	blackTurn
+	whiteWin
+	blackWin
+
+	forward action = iota
+	captureLeft
+	captureRight
+
+	pvp mode = iota
+	pvc
+	cvp
+	cvc
+)
+
+func newGame(m, n int, md mode) *game {
+	return &game{
+		b: newBoard(m, n),
+		s: whiteTurn,
+		m: md,
+	}
+}
+
+func (g *game) turn() {
+	switch g.s {
+	case whiteTurn:
+		switch g.m {
+		case pvp, pvc:
+			// player move
+		case cvp, cvc:
+			// random move by npc
+		}
+
+		g.updateState()
+	case blackTurn:
+		switch g.m {
+		case pvp, cvp:
+			// player move
+		case pvc, cvc:
+			// random move by npc
+		}
+
+		g.updateState()
+	}
+}
+
+func (g *game) play() {
+	for {
+		g.turn()
+		switch g.s {
+		case whiteWin:
+			fmt.Println("WHITE WINS")
+			break
+		case blackWin:
+			fmt.Println("BLACK WINS")
+			break
+		case illegal:
+			fmt.Println("ILLEGAL STATE")
+			break
+		case stalemate:
+			fmt.Println("STALEMATE")
+		}
+	}
+}
+
+func newBoard(m, n int) board {
+	if m < 3 || n < 3 {
+		panic("newBoard: diminsions cannot be less than three")
+	}
+
+	b := make(board, 0, m)
+
+	b = append(b, make([]pawn, 0, n))
+	for i := 0; i < n; i++ {
+		b[0] = append(b[0], blackPawn)
+	}
+
+	for i := 1; i < m-1; i++ {
+		b = append(b, make([]pawn, 0, n))
+		for j := 0; j < n; j++ {
+			b[i] = append(b[i], noPawn)
+		}
+	}
+
+	b = append(b, make([]pawn, 0, n))
+	for i := 0; i < n; i++ {
+		b[m-1] = append(b[m-1], whitePawn)
+	}
+
+	return b
+}
+
+func (b board) move(m, n int, a action) {
+	switch b[m][n] {
+	case whitePawn:
+		switch a {
+		case forward:
+			if 0 < m && b[m-1][n] == noPawn {
+				b[m-1][n] = whitePawn
+				b[m][n] = noPawn
+			}
+		case captureLeft:
+			if 0 < m && 0 < n && b[m-1][n-1] == blackPawn {
+				b[m-1][n-1] = whitePawn
+				b[m][n] = noPawn
+			}
+		case captureRight:
+			if 0 < m && n+1 < len(b[0]) && b[m-1][n+1] == blackPawn {
+				b[m-1][n+1] = whitePawn
+				b[m][n] = noPawn
+			}
+		}
+	case blackPawn:
+		switch a {
+		case forward:
+			if m+1 < len(b) && b[m][n] == noPawn {
+				b[m+1][n] = blackPawn
+				b[m][n] = noPawn
+			}
+		case captureLeft:
+			if m+1 < len(b) && n+1 < len(b[0]) && b[m+1][n+1] == whitePawn {
+				b[m+1][n+1] = blackPawn
+				b[m][n] = noPawn
+			}
+		case captureRight:
+			if m+1 < len(b) && n-1 < len(b[0]) && b[m+1][n-1] == whitePawn {
+				b[m+1][n-1] = blackPawn
+				b[m][n] = noPawn
+			}
+		}
+	}
+}
+
+func (g *game) updateState() {
+	switch g.s {
+	case whiteTurn:
+		for _, b := range g.b[0] {
+			if b == whitePawn {
+				g.s = whiteWin
+				return
+			}
+		}
+
+		g.s = blackTurn
+	case blackTurn:
+		for _, b := range g.b[len(g.b)-1] {
+			if b == blackPawn {
+				g.s = blackWin
+				return
+			}
+		}
+
+		g.s = whiteTurn
+	default:
+		g.s = illegal
+	}
+}
---
+++ positions.go
@@ -0,0 +1,115 @@
+package main
+
+var (
+	// White to move from start
+	p00 = board{
+		[]pawn{'b', 'b', 'b'},
+		[]pawn{' ', ' ', ' '},
+		[]pawn{'w', 'w', 'w'},
+	}
+
+	// FIRST MOVE
+	// White to move from p00
+	p01 = board{ // forward (2,0)
+		[]pawn{'b', 'b', 'b'},
+		[]pawn{'w', ' ', ' '},
+		[]pawn{' ', 'w', 'w'},
+	}
+	p02 = board{ // forward (2,1)
+		[]pawn{'b', 'b', 'b'},
+		[]pawn{' ', 'w', ' '},
+		[]pawn{'w', ' ', 'w'},
+	}
+	p03 = board{ // forward (2,2)
+		[]pawn{'b', 'b', 'b'},
+		[]pawn{' ', ' ', 'w'},
+		[]pawn{'w', 'w', ' '},
+	}
+
+	// SECOND MOVE
+	// Black to move from p01
+	p04 = board{ // captureRight (0,1)
+		[]pawn{'b', ' ', 'b'},
+		[]pawn{'b', ' ', ' '},
+		[]pawn{' ', 'w', 'w'},
+	}
+	p05 = board{ // forward (0,1)
+		[]pawn{'b', ' ', 'b'},
+		[]pawn{'w', 'b', ' '},
+		[]pawn{' ', 'w', 'w'},
+	}
+	p06 = board{ // forward (1,2)
+		[]pawn{'b', 'b', ' '},
+		[]pawn{'w', ' ', 'b'},
+		[]pawn{' ', 'w', 'w'},
+	}
+
+	// Black to move from p02
+	p07 = board{ // captureLeft (0,0)
+		[]pawn{' ', 'b', 'b'},
+		[]pawn{' ', 'b', ' '},
+		[]pawn{'w', ' ', 'w'},
+	}
+	p08 = board{ // forward (0,0)
+		[]pawn{' ', 'b', 'b'},
+		[]pawn{'b', 'w', ' '},
+		[]pawn{'w', ' ', 'w'},
+	}
+	p09 = board{ // captureRight (0,2)
+		[]pawn{'b', 'b', ' '},
+		[]pawn{' ', 'w', ' '},
+		[]pawn{'w', ' ', 'w'},
+	}
+	p10 = board{ // forward (0,2)
+		[]pawn{'b', 'b', ' '},
+		[]pawn{' ', 'w', 'b'},
+		[]pawn{'w', ' ', 'w'},
+	}
+
+	// Black to move from p03
+	p11 = board{ // forward (0,0)
+		[]pawn{' ', 'b', 'b'},
+		[]pawn{'b', ' ', 'w'},
+		[]pawn{'w', 'w', ' '},
+	}
+	p12 = board{ // captureLeft (0,1)
+		[]pawn{'b', ' ', 'b'},
+		[]pawn{' ', ' ', 'b'},
+		[]pawn{'w', 'w', ' '},
+	}
+	p13 = board{ // forward (0,1)
+		[]pawn{'b', ' ', 'b'},
+		[]pawn{' ', 'b', 'w'},
+		[]pawn{'w', 'w', ' '},
+	}
+
+	// THIRD MOVE
+	// White to move from p04
+	p14 = board{ // captureLeft (2,1)
+		[]pawn{'b', ' ', 'b'},
+		[]pawn{'w', ' ', ' '},
+		[]pawn{' ', ' ', 'w'},
+	}
+	p15 = board{ // forward (2,1)
+		[]pawn{'b', ' ', 'b'},
+		[]pawn{'b', 'w', ' '},
+		[]pawn{' ', ' ', 'w'},
+	}
+	p16 = board{ // forward (2,2)
+		[]pawn{'b', ' ', 'b'},
+		[]pawn{'b', ' ', 'w'},
+		[]pawn{' ', 'w', ' '},
+	}
+
+	// White to move from p05
+	p17 = board{ // captureLeft (2,0)
+		[]pawn{'b', ' ', 'b'},
+		[]pawn{'w', 'w', ' '},
+		[]pawn{' ', 'w', ' '},
+	}
+	p18 = board{ // forward (2,0)
+		[]pawn{'b', ' ', 'b'},
+		[]pawn{'w', 'b', 'w'},
+		[]pawn{' ', 'w', ' '},
+	}
+)
